1e9e1e752c7016376d93c3b71eb161d01d7dedc3
Fatimezzahradarim <fatimezzahradarim@gmail.com>
2022-08-03 12:32:48 +0200

    modification

--- briefcalculatrice.c
+++ briefcalculatrice.c
@@ -1,32 +1,46 @@
 #include <stdio.h>
 #include <stdlib.h>
-int main(){
-    int N1,N2;
-    char op,q;
-    printf("veuiller entrer le nombre N1:\n");
-    scanf("%d",&N1);
-    printf("veuiller entrer l'operation :");
-    scanf("%s",&op);
-    printf("entrer le nombre 2\n");
-    scanf("%d",&N2);
-switch (op)
+int main()
 {
-case '+': printf("%d",N1+N2);
-    break;
-case '-': printf("%d", N1-N2);
-    break; 
-case'*' : printf("%d", N1*N2);
-    break;
-case '/': if(N2!=0)
-    printf("%d",N1/N2);
-else{
-printf("impossible");}
-    break;
-case 'q':
-printf("l'opération arreter !!");   
-default:
-printf("operation non reconnu");
-    break;
-}
-}
+    int N1, N2;
+    char op, q;
+    printf("veuiller entrer le nombre N1: ");
+    scanf("%d", &N1);
+    printf("veuiller entrer l'operation: ");
+    scanf("%s", &op);
+    if (op == 'q')
+    {
+        printf("operation arreter!!");
+        exit(0);
+    }
+    else
+    {
+        printf("entrer le nombre 2: ");
+        scanf("%d", &N2);
 
+        switch (op)
+        {
+        case '+':
+            printf("%d", N1 + N2);
+            break;
+        case '-':
+            printf("%d", N1 - N2);
+            break;
+        case '*':
+            printf("%d", N1 * N2);
+            break;
+        case '/':
+            if (N2 != 0)
+                printf("%d", N1 / N2);
+            else
+            {
+                printf("impossible");
+            }
+            break;
+
+        default:
+            printf("operation non reconnu");
+            break;
+        }
+    }
+}
